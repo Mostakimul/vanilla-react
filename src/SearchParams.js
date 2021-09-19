a610ef33b0a5419532e54236b51080d80449cf31
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-09-19 20:39:58 +0200

    new breed option added

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Milan, Italy");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
   return (
     <div className="search-params">
       <form>
@@ -13,6 +18,40 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+            id="animal"
+            value={animal}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+            id="breed"
+            value={breed}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
 
         <button>Submit</button>
