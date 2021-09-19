d3172f8bd18aefbb1c3dc3133af46d96429db6ae
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-09-19 19:22:34 +0200

    react configure with eslint

--- src/App.js
+++ src/App.js
@@ -1,31 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
-
-/*
-// sample h1 rendering
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Sample React"),
-    // ...[1,2,3,4].map((i) => React.createElement("h2", {}, i)),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog!!!",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Tiger",
-      animal: "Cat",
-      breed: "tiger",
-    }),
-    React.createElement(Pet, {
-      name: "Sweety",
-      animal: "Tiger",
-      breed: "Bengal",
-    }),
-  ]);
-};
-*/
 
 const App = () => {
   return (
@@ -38,4 +12,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,15 +1,3 @@
-import React from "react";
-/*
-// Pet component
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h3", {}, props.name),
-    React.createElement("h4", {}, props.animal),
-    React.createElement("h4", {}, props.breed),
-  ]);
-};
-*/
-
 const Pet = (props) => {
   return (
     <div>
